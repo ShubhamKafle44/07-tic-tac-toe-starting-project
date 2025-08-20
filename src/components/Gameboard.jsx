5f66314124b680b4c9596e6a5640c11a6e19fa0e
shubhamkafle44 <shubhamkafle44@gmail.com>
2025-08-20 13:02:01 -0500

    Fix the issue of clicking the same square twice

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -25,7 +25,7 @@ const Gameboard = ({ onSelectSquare, turns }) => {
         <ol id='game-board'>
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
-                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button></li>)}
+                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol != null}>{playerSymbol}</button></li>)}
                 </ol>
             </li>)}
         </ol>
